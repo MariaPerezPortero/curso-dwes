1bd2243c8628578f8a2910e26d7256369c9b0bea
Maria <perez.maria.portero@gmail.com>
2026-05-04 23:58:32 +0200

    Tarea07

--- UT-07/click_to_edit/main/models.py
+++ UT-07/click_to_edit/main/models.py
@@ -3,6 +3,7 @@ class MockUser:
         self.first = "Juan"
         self.last = "Pérez"
         self.email = "juan@example.com"
+        self.numero_telefono = "600123456"
 
 db = MockUser()
 
@@ -11,8 +12,9 @@ def get_user():
     return db
 
 
-def update_user(first, last, email):
+def update_user(first, last, email, numero_telefono):
     db.first = first
     db.last = last
     db.email = email
+    db.numero_telefono = numero_telefono
     return db
